60b5f9e19af0a7f43e369db73a77c54de915ddc1
Mateusz Sadowski <mateuszsadowski@Mateuszs-MacBook-Pro.local>
2020-03-14 14:48:44 +0100

    Add make moves sequentially

--- client.py
+++ client.py
@@ -5,59 +5,151 @@ import string
 import time
 import datetime
 
-RECV_LEN = 1000000
-port = 8080
+# RECV_LEN = 4096
+RECV_LEN = 10000000
+PORT = 8080
+IP = 'localhost'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect(("localhost", port))
+GAME_ID = 41
+
+player = {
+    'id': -1,
+    'username': ''
+}
+session = {
+    'start': -1,
+    'fields': {},
+    'players': [],
+    'next_turn': -1
+}
+
+# Create socket and connect
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket successfully created')
+except socket.error as err:
+    print('Socket creation failed with error {0}'.format(err))
+
+try:
+    s.connect((IP, PORT))
+    print('Socket successfully connected to {0} on port {1}'.format(IP, PORT))
+except socket.error as err:
+    print('Socket connection failed with error {0}'.format(err))
+
+# def receive():
+#     response = ''
+#     while True:
+#         data = s.recv(RECV_LEN)
+#         if not data:
+#             break
+#         response += data
+#         messages = response.split('\r\n')
+#     return messages
+
+def removeValuesFromList(the_list, val):
+   return [value for value in the_list if value != val]
 
 def randomString(stringLength=10):
-    """Generate a random string of fixed length """
+    # Generate a random string of fixed length
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-username = randomString()
-game_id = 3
+def receive():
+    response = s.recv(RECV_LEN)
+    if response == '':
+        print('Received an empty reponse')
+        return -1
+    print('Received: ' + response)
+    messages = response.split('\r\n')
+    messages = removeValuesFromList(messages, '')
+    return messages
 
-user = {"username": username, "gameID": game_id};
+def handlePlayer(msg_info):
+    player['id'] = msg_info['id']
+    player['username'] = msg_info['username']
+
+def handleInit(msg_info):
+    # print(msg_info['session'])
+    session['start'] = msg_info['session']['start']
+    session['fields'] = msg_info['session']['fields']
+    session['players'] = msg_info['session']['start']
+
+def handleTurn(msg_info):
+    session['next_turn'] = msg_info['playerID']
+
+def handleMessage(msg):
+    msg_info = json.loads(msg)
+    # TODO: implement switch
+    # switcher = {
+    #     u'player': handlePlayer(msg_info)
+    # }
+    # func = switcher.get(msg_info['type'], lambda: 'Invalid message type')
+    # return func
+    msg_type = msg_info['type']
+    if msg_type == 'player':
+        handlePlayer(msg_info)
+    elif msg_type == 'init':
+        handleInit(msg_info)
+    elif msg_type == 'turn':
+        handleTurn(msg_info)
+    else:
+        print('Invalid message type')
+
+def handleMessages(messages):
+    for msg in messages:
+        handleMessage(msg)
+
+def send(msg):
+    print('Sending: ' + msg)
+    time.sleep(1)
+    return s.send(msg)
+
+# Login
+username = randomString()
+user = {'username': username, 'gameID': GAME_ID}
 login_msg = json.dumps(user)
 
-print("Sending: " + login_msg)
-time.sleep(1)
-s.send(login_msg)
+send(login_msg)
 
-login_response = s.recv(RECV_LEN)
-print("Recieved: " + login_response)
-user_info = json.loads(login_response)
-user_id = user_info["id"]
+messages = receive()
+if(messages != -1):
+    handleMessages(messages)
 
-print("Waiting for game to start...")
-initial_config = json.loads(s.recv(RECV_LEN))
-turn_of_player = json.loads(s.recv(RECV_LEN))
+# Game start
+print('Waiting for game to start...')
+messages = receive()
+if(messages != -1):
+    handleMessages(messages)
 
-print("Board state: \n")
-print(initial_config["session"])
-player_id = turn_of_player["playerID"]
-print("Turn of player with id: ")
-print(player_id)
+print('Board state:')
+print(session)
 
-print("User ID: ")
-print(user_id)
-if user_id == player_id:
-    time.sleep(1)
-    print("My move!")
-    move = { "createdAt": datetime.datetime.now().isoformat() + "+00:00", "oldFieldID": 92, "newFieldID": 58 }
-    # move = { "oldFieldID": 69, "newFieldID": 2 }
-    move_msg = json.dumps(move)
-    print("Sent: ")
-    print(s.send(move_msg))
+while(session['next_turn'] == -1):
+    messages = receive()
+    if(messages != -1):
+        handleMessages(messages)
+print('Turn of player with id: ')
+print(session['next_turn'])
 
-response = s.recv(RECV_LEN)
-print(response)
+# Wait for turn or make move
+while True:
+    print('User ID: ')
+    print(player["id"])
+    if player["id"] == session['next_turn']:
+        time.sleep(3)
+        print('My move!')
+        move = { 'createdAt': datetime.datetime.now().isoformat() + '+00:00', 'oldFieldID': 63, 'newFieldID': 2 }
+        # move = { 'oldFieldID': 69, 'newFieldID': 2 }
+        move_msg = json.dumps(move)
+        send(move_msg)
+
+    messages = receive()
+    handleMessages(messages)
+
 # next_config = json.loads(s.recv(RECV_LEN))
 # next_turn_of_player = json.loads(s.recv(RECV_LEN))
 
-# print("Board state: \n")
-# print(next_config["session"])
-# player_id = next_turn_of_player["playerID"]
-# print("Turn of player with id: " + player_id + "\n")
+# print('Board state: \n')
+# print(next_config['session'])
+# player_id = next_turn_of_player['playerID']
+# print('Turn of player with id: ' + player_id + '\n')
